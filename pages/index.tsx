1ab6a4cefea8fe12ec507bf0b3a614d6331385f2
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-13 14:51:30 +0100

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,31 +11,31 @@ import { motion } from "framer-motion"
 import { fadeInUp, routeAnimation, stagger } from "../styles/animation";
 import Head from "next/head"
 
-const About = ({endpoint}) => {
+const About = ({ endpoint }) => {
 
   console.log(endpoint);
 
   return (
-    <motion.div className="flex flex-col flex-grow px-6 pt-1 " 
-    variants={routeAnimation}
-    initial="initial"
-    animate="animate"
-    exit="exit" >
+    <motion.div className="flex flex-col flex-grow px-6 pt-1 "
+      variants={routeAnimation}
+      initial="initial"
+      animate="animate"
+      exit="exit" >
 
 
-     <Head>
-       <title>
-           Web Developer | portfolio | Alao Abdul-Quayyum | Quayyum | MERN Stack
-       </title>
-     </Head>
+      <Head>
+        <title>
+          Web Developer | portfolio | Alao Abdul-Quayyum | Quayyum | MERN Stack
+        </title>
+      </Head>
 
 
       <h6 className="my-3 text-base font-medium">
-        I am  Software Developer using JavaScript || TypeScript || MERN || Flutter || React-Native Technologies to solve problems,
+        I am a Software Developer using JavaScript || TypeScript || React.Js || Vue.Js || Next.Js || Express.Js || Node.Js || Flutter || React-Native Technologies to solve problems,
         I am currently pursuing B.Sc Degree
         in Computer Science from University Of Ilorin. I have 1+ year
         experience in Web Development.
-     </h6>
+      </h6>
       <div
         className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
         style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
@@ -44,10 +44,10 @@ const About = ({endpoint}) => {
           What I Do
         </h4>
 
-        <motion.div className="grid gap-8 my-3 md:grid-cols-1" 
-        variants={stagger}
-        initial="initial" 
-        animate="animate">
+        <motion.div className="grid gap-8 my-3 md:grid-cols-1"
+          variants={stagger}
+          initial="initial"
+          animate="animate">
           {/* children's initial and animate property should be same as the parent during a stagger effect  */}
           {services.map((service) => (
             <motion.div
@@ -66,15 +66,15 @@ const About = ({endpoint}) => {
 
 
 //!called every time  the page refreshed
- export const getServerSideProps: GetServerSideProps = async (
-    context: GetServerSidePropsContext
- ) => {
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   //  console.log(process.env.VERCEL_URL);
   //   const res = await fetch('${process.env.VERCEL_URL}/api/services')
   //   const data = await res.json()
   //  console.log(data)
-   return { props: { endpoint: process.env.VERCEL_URL } }
- }
+  return { props: { endpoint: process.env.VERCEL_URL } }
+}
 
 //!called only during the build of the project
 //? make sure the server(localhost:3000)[this will receive the request during build] is running on a terminal during the build
